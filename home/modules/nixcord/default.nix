5cd7172c47ecb5d759f2986e607783e34adb8f15
ctrl-kitty <104305684+ctrl-kitty@users.noreply.github.com>
2026-02-14 20:26:35 +0300

    discord removed + vpn rules

--- home/modules/nixcord/default.nix
+++ home/modules/nixcord/default.nix
@@ -4,11 +4,6 @@
     enable = true;
     # Do not install basic discord
     #discord.enable = false;
-	discord = {
-      enable = true;
-      vencord.enable = false;
-      equicord.enable = true;
-    };
     # equibop.enable = true;
 	# vencord.enable = false;
 	# equicord.enable = true;
