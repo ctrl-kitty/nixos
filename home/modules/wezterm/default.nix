72309929e86a0d24b462c807a89b719f0deb5c10
ctrl-kitty <104305684+ctrl-kitty@users.noreply.github.com>
2026-07-11 02:16:15 +0300

    Ayugram portals fix

--- home/modules/wezterm/default.nix
+++ home/modules/wezterm/default.nix
@@ -1,7 +1,8 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.wezterm = {
     enable = true;
+	package = pkgs.unstable.wezterm;
     enableZshIntegration = true;
     extraConfig = builtins.readFile ./wezterm.lua;
   };
